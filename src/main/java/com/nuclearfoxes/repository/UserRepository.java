5ca20888f394a34f75a6552a96a91814e538c3e9
Zhekuson <zhekuson@gmail.com>
2020-11-18 22:49:35 +0300

    jar added

--- src/main/java/com/nuclearfoxes/repository/UserRepository.java
+++ src/main/java/com/nuclearfoxes/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.Repository;
 import java.util.List;
 
 
+
 public interface UserRepository extends Repository<User,Integer> {
     @Query(nativeQuery = true,value = "SELECT * FROM users")
     List<User> getAllUsers();
